d5632b62911b3659ab7c21d8156d80066bbb09ac
Noah Rodriguez <nrodrig3@valpo.edu>
2024-08-30 16:47:34 -0500

    Adding Google auth provider

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,10 +1,29 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-export async function updateUser(): Promise<void> {
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  email: string,
+  path: string
+): Promise<void> {
   connectToDB();
 
-  await User.findOneAndUpdate();
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      { username: username.toLowerCase(), name, email },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Fialed to create/update user: ${error.message}`);
+  }
 }
